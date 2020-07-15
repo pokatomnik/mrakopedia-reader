a1684384f1b6f2ac1ebb1268d91ff39096246144
Danilian Akhmedzianov <hugefast@gmail.com>
2020-07-16 01:54:49 +0300

    make items highlightable

--- app/src/main/java/com/example/mrakopediareader/PageResultsAdapter.java
+++ app/src/main/java/com/example/mrakopediareader/PageResultsAdapter.java
@@ -2,6 +2,7 @@ package com.example.mrakopediareader;
 
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
@@ -14,10 +15,10 @@ class PageResultsAdapter extends RecyclerView.Adapter<PageResultsAdapter.ViewHol
     private ArrayList<Page> mDataset;
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        private TextView textView;
-        public ViewHolder(TextView v) {
+        private LinearLayout layout;
+        public ViewHolder(LinearLayout v) {
             super(v);
-            textView = v;
+            layout = v;
         }
     }
 
@@ -27,15 +28,16 @@ class PageResultsAdapter extends RecyclerView.Adapter<PageResultsAdapter.ViewHol
 
     @Override
     public PageResultsAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        TextView v = (TextView) LayoutInflater.from(parent.getContext())
+        LinearLayout layout = (LinearLayout) LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.search_result_view, parent, false);
-        ViewHolder vh = new ViewHolder(v);
+        ViewHolder vh = new ViewHolder(layout);
         return vh;
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        holder.textView.setText(mDataset.get(position).getTitle());
+        final TextView textView = holder.layout.findViewById(R.id.pageTitle);
+        textView.setText(mDataset.get(position).getTitle());
 
     }
 
