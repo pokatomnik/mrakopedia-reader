f7d62da92fbd914abe9ce8fad172f82ef3dad9e8
Danilian Akhmedzianov <hugefast@gmail.com>
2020-07-28 01:21:59 +0300

    sort favorites alphabetically

--- app/src/main/java/com/example/mrakopediareader/PagesList/FavoritesList.java
+++ app/src/main/java/com/example/mrakopediareader/PagesList/FavoritesList.java
@@ -4,6 +4,7 @@ import com.example.mrakopediareader.FavoritesStore;
 import com.example.mrakopediareader.api.Page;
 
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 import io.reactivex.rxjava3.core.Observable;
 
@@ -11,7 +12,11 @@ public class FavoritesList extends PagesList {
     @Override
     protected Observable<ArrayList<Page>> getPages() {
         final FavoritesStore favoritesStore = new FavoritesStore(getBaseContext());
-        final ArrayList<Page> pages = new ArrayList<>(favoritesStore.getPages());
-        return Observable.just(pages);
+        final ArrayList<Page> pagesSorted = favoritesStore
+                .getPages()
+                .stream()
+                .sorted((page1, page2) -> page1.getTitle().compareTo(page2.getTitle()))
+                .collect(Collectors.toCollection(ArrayList::new));
+        return Observable.just(pagesSorted);
     }
 }
