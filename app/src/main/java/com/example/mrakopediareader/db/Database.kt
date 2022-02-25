5510d7c2ccb8b8e797780f314292538a0160441c
Danilian Akhmedzianov <hugefast@gmail.com>
2022-02-26 00:29:33 +0300

    use database migrations approach

--- app/src/main/java/com/example/mrakopediareader/db/Database.kt
+++ app/src/main/java/com/example/mrakopediareader/db/Database.kt
@@ -9,7 +9,16 @@ import com.example.mrakopediareader.db.dao.recent.RecentDao
 import com.example.mrakopediareader.db.dao.scrollposition.ScrollPosition
 import com.example.mrakopediareader.db.dao.scrollposition.ScrollPositionDao
 
-@Database(entities = [Favorite::class, ScrollPosition::class, Recent::class], version = 2, exportSchema = false)
+@Database(
+    entities = [Favorite::class, ScrollPosition::class, Recent::class],
+    version = 1,
+//    https://developer.android.com/training/data-storage/room/migrating-db-versions#groovy
+//    autoMigrations = [
+//        AutoMigration (from = 1, to = 2),
+//        AutoMigration (from = 2, to = 3)
+//    ],
+    exportSchema = true,
+)
 abstract class Database : RoomDatabase() {
     abstract fun favoritesDao(): FavoritesDao
 
